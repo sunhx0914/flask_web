345f2449a072546843e36d22b8315a2f9c8f23eb
sunhx0914 <15232136986@163.com>
2020-03-31 10:59:43 +0800

    first commit

---
+++ demos/hello/app.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+    :author: Grey Li (李辉)
+    :url: http://greyli.com
+    :copyright: © 2018 Grey Li
+    :license: MIT, see LICENSE for more details.
+"""
+import click
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# the minimal Flask application
+@app.route('/')
+def index():
+    return '<h1>Hello, World!</h1>'
+
+
+# bind multiple URL for one view function
+@app.route('/hi')
+@app.route('/hello')
+def say_hello():
+    return '<h1>Hello, Flask!</h1>'
+
+
+# dynamic route, URL variable default
+@app.route('/greet', defaults={'name': 'Programmer'})
+@app.route('/greet/<name>')
+def greet(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+# 等同于 将默认值写做参数
+# @app.route('/greet')
+# @app.route('/greet/<name>')
+# def greet(name='Programmer'):
+#     return '<h1>Hello, %s!</h1>' % name
+
+# custom flask cli command
+@app.cli.command()
+def hello():
+    """Just say hello."""
+    click.echo('Hello, Human!')
+
+# 也可以这样设置名字
+@app.cli.command('say-hello')
+def hello_say():
+    """Just say hello."""
+    click.echo('say-Hello, Human!')
