675f4c8fd9717c7c59cf3757e5c54c1ff9eb50ef
sunhx0914 <15232136986@163.com>
2020-04-01 22:19:44 +0800

    modify form

--- demos/form/app.py
+++ demos/form/app.py
@@ -23,15 +23,17 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
 # Custom config
-app.config['UPLOAD_PATH'] = os.path.join(app.root_path, 'uploads')
+app.config['UPLOAD_PATH'] = os.path.join(app.root_path, 'uploads')  # 上传文件保存位置  需手动创建文件夹
+# app.root_path 存储了程序实例所在脚本的绝对路径
 app.config['ALLOWED_EXTENSIONS'] = ['png', 'jpg', 'jpeg', 'gif']
 
 # Flask config
-# set request body's max length
+# set request body's max length 设置上传大小 单位 字节byte
+# flask内置的开服务器会中断连接，在生产环境的服务器上会返回413错误响应。
 # app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024  # 3Mb
 
 # Flask-CKEditor config
-app.config['CKEDITOR_SERVE_LOCAL'] = True
+app.config['CKEDITOR_SERVE_LOCAL'] = True   # 使用内置的本地资源
 app.config['CKEDITOR_FILE_UPLOADER'] = 'upload_for_ckeditor'
 
 # Flask-Dropzone config
@@ -39,7 +41,7 @@ app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image'
 app.config['DROPZONE_MAX_FILE_SIZE'] = 3
 app.config['DROPZONE_MAX_FILES'] = 30
 
-ckeditor = CKEditor(app)
+ckeditor = CKEditor(app)    # 实例化CKEditor类
 dropzone = Dropzone(app)
 
 
@@ -48,6 +50,7 @@ def index():
     return render_template('index.html')
 
 
+# flask 默认为GET 所以需要methods=['GET', 'POST']
 @app.route('/html', methods=['GET', 'POST'])
 def html():
     form = LoginForm()
@@ -61,13 +64,16 @@ def html():
 @app.route('/basic', methods=['GET', 'POST'])
 def basic():
     form = LoginForm()
-    if form.validate_on_submit():
-        username = form.username.data
+    if form.validate_on_submit():   # 验证 提交表单 验证数据
+        username = form.username.data   # data 属性会匹配所有字段与对应数据的字典
+        # form.字段属性名.data
         flash('Welcome home, %s!' % username)
-        return redirect(url_for('index'))
-    return render_template('basic.html', form=form)
+        return redirect(url_for('index'))   # PRG技术:防止重复提交表单
+        # 通过重定向 将最后的请求改为GET
+    return render_template('basic.html', form=form)     # 实例化表单类 再将表单实例传入模板
 
 
+# 这个是 在调用字段时传入参数来定义额外的属性 与basic相比
 @app.route('/bootstrap', methods=['GET', 'POST'])
 def bootstrap():
     form = LoginForm()
@@ -78,6 +84,7 @@ def bootstrap():
     return render_template('bootstrap.html', form=form)
 
 
+# 获取数据 - 数据转换 - 验证数据 - 返回错误 - 通过验证，保存
 @app.route('/custom-validator', methods=['GET', 'POST'])
 def custom_validator():
     form = FortyTwoForm()
@@ -87,9 +94,11 @@ def custom_validator():
     return render_template('custom_validator.html', form=form)
 
 
+# 显示上传的文件 / 返回静态文件
 @app.route('/uploads/<path:filename>')
 def get_file(filename):
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
+    # send_from_directory 获取文件   传入路径和文件名
 
 
 @app.route('/uploaded-images')
@@ -97,6 +106,7 @@ def show_images():
     return render_template('uploaded.html')
 
 
+# 验证文件类型
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
@@ -104,23 +114,27 @@ def allowed_file(filename):
 
 def random_filename(filename):
     ext = os.path.splitext(filename)[1]
-    new_filename = uuid.uuid4().hex + ext
+    new_filename = uuid.uuid4().hex + ext   # 使用uuid生成新文件名，并用hex获取16进制字符串，再加后缀
+    # uuid 有5个版本 方法不同，对应场景不同
     return new_filename
 
 
+# 上传文件
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     form = UploadForm()
-    if form.validate_on_submit():
-        f = form.photo.data
-        filename = random_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-        flash('Upload success.')
-        session['filenames'] = [filename]
-        return redirect(url_for('show_images'))
+    if form.validate_on_submit():   # 通过验证后执行以下操作：
+        f = form.photo.data     # 自动获取文件对象
+        filename = random_filename(f.filename)  # f.filename 原文件名
+        f.save(os.path.join(app.config['UPLOAD_PATH'], filename))   # 保存文件
+        flash('Upload success.')    # 发送提示
+        session['filenames'] = [filename]   # 将文件名存入session 为了兼容多文件上传
+        return redirect(url_for('show_images'))     # 重定向到 show_images
     return render_template('upload.html', form=form)
 
 
+# Flask-WTF 0.14.2 未添加多文件上传渲染和验证的支持
+# 客户端是 input 是如何 添加 multiple 属性的？ 与单文件相比
 @app.route('/multi-upload', methods=['GET', 'POST'])
 def multi_upload():
     form = MultiUploadForm()
@@ -128,7 +142,7 @@ def multi_upload():
     if request.method == 'POST':
         filenames = []
 
-        # check csrf token
+        # check csrf token 验证CSRF令牌
         try:
             validate_csrf(form.csrf_token.data)
         except ValidationError:
@@ -136,11 +150,15 @@ def multi_upload():
             return redirect(url_for('multi_upload'))
 
         # check if the post request has the file part
+        # 用来确保字段中包含文件数据 相当于 FileRequired() 验证器
         if 'photo' not in request.files:
             flash('This field is required.')
             return redirect(url_for('multi_upload'))
 
         for f in request.files.getlist('photo'):
+            # request.files 调用 getlist() 并传入字段的name属性值
+            # request.files.getlist('photo') 会返回包含所有上传文件对象的list
+
             # if user does not select file, browser also
             # submit a empty part without filename
             # if f.filename == '':
@@ -148,17 +166,18 @@ def multi_upload():
             #    return redirect(url_for('multi_upload'))
             # check the file extension
             if f and allowed_file(f.filename):
+                # allowed_file() 相当于 FileAllowed() 验证器
                 filename = random_filename(f.filename)
                 f.save(os.path.join(
                     app.config['UPLOAD_PATH'], filename
                 ))
-                filenames.append(filename)
+                filenames.append(filename)  # 保存文件名到session的 列表
             else:
                 flash('Invalid file type.')
                 return redirect(url_for('multi_upload'))
         flash('Upload success.')
         session['filenames'] = filenames
-        return redirect(url_for('show_images'))
+        return redirect(url_for('show_images'))     # 会获取所有文件名，渲染所有上传图像
     return render_template('upload.html', form=form)
 
 
@@ -180,11 +199,12 @@ def dropzone_upload():
     return render_template('dropzone.html')
 
 
+# 两个提交
 @app.route('/two-submits', methods=['GET', 'POST'])
 def two_submits():
     form = NewPostForm()
     if form.validate_on_submit():
-        if form.save.data:
+        if form.save.data:  # 点击的按钮为True 未点击为False
             # save it...
             flash('You click the "Save" button.')
         elif form.publish.data:
@@ -194,11 +214,13 @@ def two_submits():
     return render_template('2submit.html', form=form)
 
 
+# 两个表单 单视图
 @app.route('/multi-form', methods=['GET', 'POST'])
 def multi_form():
     signin_form = SigninForm()
     register_form = RegisterForm()
 
+    # signin_form.submit1.data 点击为True 未点击为False
     if signin_form.submit1.data and signin_form.validate():
         username = signin_form.username.data
         flash('%s, you just submit the Signin Form.' % username)
@@ -212,14 +234,16 @@ def multi_form():
     return render_template('2form.html', signin_form=signin_form, register_form=register_form)
 
 
-@app.route('/multi-form-multi-view')
-def multi_form_multi_view():
+# 两个表单 多视图
+# 渲染包含表单的模板(GET)、处理表单请求(POST) 解耦 渲染放在单独视图
+@app.route('/multi-form-multi-view')    # 只负责GET
+def multi_form_multi_view():    # 实例化两个表单类 并渲染模板
     signin_form = SigninForm2()
     register_form = RegisterForm2()
     return render_template('2form2view.html', signin_form=signin_form, register_form=register_form)
 
 
-@app.route('/handle-signin', methods=['POST'])
+@app.route('/handle-signin', methods=['POST'])  # 单独视图处理POST
 def handle_signin():
     signin_form = SigninForm2()
     register_form = RegisterForm2()
@@ -243,6 +267,8 @@ def handle_register():
         return redirect(url_for('index'))
     return render_template('2form2view.html', signin_form=signin_form, register_form=register_form)
 
+
+# 看上述方法的缺点   p136
 
 @app.route('/ckeditor', methods=['GET', 'POST'])
 def integrate_ckeditor():
--- demos/form/forms.py
+++ demos/form/forms.py
@@ -14,19 +14,23 @@ from wtforms.validators import DataRequired, Length, ValidationError, Email
 
 
 # 4.2.1 basic form example
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+class LoginForm(FlaskForm):     # Field 字段       实例化字段类参数 p107
+    username = StringField('Username', validators=[DataRequired()])     # 文本字段
     password = PasswordField('Password', validators=[DataRequired(), Length(8, 128)])
-    remember = BooleanField('Remember me')
-    submit = SubmitField('Log in')
+    # 密码字段  validators为一个列表，包含一系列验证器
+    remember = BooleanField('Remember me')     # 复选框字段
+    submit = SubmitField('Log in')     # 提交按钮字段
+    # 每个字段属性通过实例化WTForms提供的字段类表示，属性名称将作为HTML<input>的name及id属性值,小写的那个
+    # 大小写敏感 不能以 _ 和 validate 开头
 
 
-# custom validator
+# custom validator 自定义验证器
 class FortyTwoForm(FlaskForm):
     answer = IntegerField('The Number')
     submit = SubmitField()
 
-    def validate_answer(form, field):
+    def validate_answer(form, field):   # 方法命名为 validate_字段属性名 会自动调用其进行验证
+        # form 为表单实例  field 为字段对象
         if field.data != 42:
             raise ValidationError('Must be 42.')
 
@@ -34,6 +38,7 @@ class FortyTwoForm(FlaskForm):
 # upload form
 class UploadForm(FlaskForm):
     photo = FileField('Upload Image', validators=[FileRequired(), FileAllowed(['jpg', 'jpeg', 'png', 'gif'])])
+    # 上传字段    FileRequired() 是否包含文件对象    FileAllowed()验证文件类型，允许的后缀名list
     submit = SubmitField()
 
 
@@ -44,19 +49,22 @@ class MultiUploadForm(FlaskForm):
 
 
 # multiple submit button
-class NewPostForm(FlaskForm):
+class NewPostForm(FlaskForm):   # 两个提交按钮
     title = StringField('Title', validators=[DataRequired(), Length(1, 50)])
     body = TextAreaField('Body', validators=[DataRequired()])
     save = SubmitField('Save')
     publish = SubmitField('Publish')
 
 
+# 为两个表单的提交字段设置不同的名字  思路 通过提交按钮判断
+# 登陆表单
 class SigninForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
     password = PasswordField('Password', validators=[DataRequired(), Length(8, 128)])
     submit1 = SubmitField('Sign in')
 
 
+# 注册表单
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
     email = StringField('Email', validators=[DataRequired(), Email(), Length(1, 254)])
@@ -64,6 +72,7 @@ class RegisterForm(FlaskForm):
     submit2 = SubmitField('Register')
 
 
+# 思路 分离表单的渲染和验证 表单提交可用同一个名称
 class SigninForm2(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(1, 24)])
     password = PasswordField('Password', validators=[DataRequired(), Length(8, 128)])
@@ -77,7 +86,7 @@ class RegisterForm2(FlaskForm):
     submit = SubmitField()
 
 
-# CKEditor Form
+# CKEditor Form 文章表单
 class RichTextForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired(), Length(1, 50)])
     body = CKEditorField('Body', validators=[DataRequired()])
